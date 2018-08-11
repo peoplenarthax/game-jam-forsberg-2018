82e2561163dab48f78756950476a2d035489f031
indianaj0nas <31271188+indianaj0nas@users.noreply.github.com>
2018-08-11 21:51:21 +0200

    lots of stuff
    New enemy with boxing glove and spikes and car and more scene stuff

---
+++ GameJam2018Aug/Assets/Arts/FollowAnchor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAnchor : MonoBehaviour {
+
+	public GameObject anchor;
+
+	void Update () 
+	{
+		transform.position = anchor.transform.position;
+	}
+}
